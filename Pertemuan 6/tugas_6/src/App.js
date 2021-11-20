901c3fae19ce021b4c0a667006439f15d1add76b
ShellaYolanda <192110681@students.mikroskil.ac.id>
2021-11-20 11:13:04 +0700

    pertemuan 6

--- Pertemuan 6/tugas_6/src/App.js
+++ Pertemuan 6/tugas_6/src/App.js
@@ -1,57 +1,136 @@
-import React, {Component} from 'react';
-import './css/bootstrap.min.css';
-import Foto from "./Foto.jpg";
+import React, { Component } from "react";
+import "./App.css";
 import {
   BrowserRouter as Router,
-  NavLink,
   Route,
   Switch,
   Redirect,
   withRouter,
+  NavLink,
 } from "react-router-dom";
+import "./css/bootstrap.min.css";
+import Foto from "./Foto.jpg";
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      isAuth : false
-    }
+  constructor() {
+    super();
+    this.state = { isAuth: false };
   }
 
-  render(){
-      const LoginButton = withRouter (({history}) =>(
-        <button onClick={()=>{
-          this.setState({isAuth : true})
-          history.push('/profile')
-        }}>Login</button>
-      ))
-      const LogoutButton = withRouter (({history}) =>(
-        <button onClick={()=>{
-          this.setState({isAuth : false})
-          history.push('/login')
-        }}>Logout</button>
-      ))
-      return(
+  render() {
+    const LoginButton = withRouter(({ history }) => (
+      <button
+        onClick={() => {
+          this.setState({ isAuth: true });
+          history.push("/profile");
+        }} 
+      >
+        Login
+      </button>
+    ));
+
+    const LogoutButton = withRouter(({ history }) => (
+      <button
+        onClick={() => {
+          this.setState({ isAuth: false });
+          history.push("/login");
+        }}
+      >
+        Logout
+      </button>
+    ));
+
+    const routes = [
+      {
+        path: "/",
+        exact: true,
+        render: () => <div className="container text-center font-weight-bold">Welcome to my Home</div>,
+      },
+      {
+        path: "/login",
+        render: () => <LoginButton />,
+      },
+      {
+        path: "/profile",
+        render: () =>
+          this.state.isAuth ? (
+            <div>
+              <h1 className="text-center">Profile Mahasiswa</h1>
+              <div className="container text-center">
+                <table className="table table-borderless text-start">
+                  <tbody>
+                    <tr>
+                      <td>Foto Profil</td>
+                      <td>:</td>
+                      <td>
+                        <img src={Foto} className="w-25 h-5 border border-1" />
+                      </td>
+                    </tr>
+                    <tr>
+                      <td>Nama</td>
+                      <td>:</td>
+                      <td>Yang yang</td>
+                    </tr>
+                    <tr>
+                      <td>NIM</td>
+                      <td>:</td>
+                      <td>19211234</td>
+                    </tr>
+                    <tr>
+                      <td>Jenis Kelamin</td>
+                      <td>:</td>
+                      <td>Laki-Laki</td>
+                    </tr>
+                    <tr>
+                      <td>Tempat Tanggal Lahir</td>
+                      <td>:</td>
+                      <td>Medan, 9 September 1991</td>
+                    </tr>
+                    <tr>
+                      <td></td>
+                      <td></td>
+                      <td><LogoutButton /></td>
+                    </tr>
+                  </tbody>
+                </table>
+                <br />
+              </div>
+            </div>
+          ) : (
+            <Redirect to="/login" />
+          ),
+      },
+    ];
+
+    return (
+      <div>
         <Router>
           <div>
-            <ul style={{listStyle: 'none'}}>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/news'>News</Link></li>
-              <li><Link to='/profile'>Profile</Link></li>
-            </ul>
-  
-            <Switch>
-              <Route path='/' exact render={()=> <div>Ini halaman Home</div>} />
-              <Route path='/news' render={()=> <div>Ini halaman News</div>} />
-              <Route path='/login' render={()=> <LoginButton />} />
-              <Route path='/profile' render={()=> this.state.isAuth ? <div>Ini 
-                halaman Profile <br /> <LogoutButton /> </div> : <Redirect
-                to='/login'/>} />
-            </Switch>
+            <nav className="navbar navbar-expand-sm bg-dark navbar-dark">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <NavLink exact className="nav-link" to="/">
+                    Home
+                  </NavLink>
+                </li>
+                <li className="nav-item">
+                  <NavLink exact className="nav-link" to="/profile">
+                    Profile
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
           </div>
+
+          <Switch>
+            {routes.map((item, index) => (
+              <Route path={item.path} exact={item.exact} render={item.render} />
+            ))}
+          </Switch>
         </Router>
-      )
-    }
+      </div>
+    );
+  }
 }
 
 export default App;
